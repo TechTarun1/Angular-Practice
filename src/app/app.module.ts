aba6a0f8f8859e895608e3b47e5dd814567adebf
Tarun <Tarun.Boddeboyina@wallero.com>
2023-06-04 18:53:20 +0530

    working on angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HomeComponent } from './home/home.component';
+import { HomeComponent } from '../app/components/home/home.component';
+import { RegistrationComponent } from './components/registration/registration.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    RegistrationComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    NgbModule,
+    MatSlideToggleModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
