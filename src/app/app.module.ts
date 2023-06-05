088b517a0ffe5fb421eb5866eecb856ae03f0c91
Tarun <Tarun.Boddeboyina@wallero.com>
2023-06-05 14:43:14 +0530

    included stepper

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatStepperModule } from '@angular/material/stepper';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,12 +9,18 @@ import { HomeComponent } from '../app/components/home/home.component';
 import { RegistrationComponent } from './components/registration/registration.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { TableComponent } from './components/table/table.component';
+import { CarousalComponent } from './components/carousal/carousal.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    RegistrationComponent
+    RegistrationComponent,
+    NavbarComponent,
+    TableComponent,
+    CarousalComponent
   ],
   imports: [
     BrowserModule,
@@ -21,6 +28,7 @@ import { MatSlideToggleModule } from '@angular/material/slide-toggle';
     BrowserAnimationsModule,
     NgbModule,
     MatSlideToggleModule,
+    MatStepperModule
   ],
   providers: [],
   bootstrap: [AppComponent]
