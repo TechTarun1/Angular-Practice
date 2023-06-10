00e675b61439f8f47e098c91a4889102390f4e60
Tarun <Tarun.Boddeboyina@wallero.com>
2023-06-10 18:45:16 +0530

    worked on counter app

--- src/app/components/counter-app/counter-app.component.ts
+++ src/app/components/counter-app/counter-app.component.ts
@@ -6,8 +6,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./counter-app.component.css']
 })
 export class CounterAppComponent {
-  count = 0
-  counter(type: string) {
-    type === 'add' ? this.count++ : this.count--;
+  count: number = 0
+  isActive: boolean = true
+  status: string = ''
+
+  counter = (type: string) => {
+    type === 'add' ? (this.count++) : this.count--;
+  }
+
+  profitOrLoss = () => {
+    console.log(this.count)
+    let className = ''
+    if (this.count > 10) {
+      // this.isActive = true
+      className = 'profit'
+      this.status = 'Profit'
+    }
+    else if (this.count < 0) {
+      // this.isActive = false
+      className = 'loss'
+      this.status = 'Loss'
+    }
+    else{
+      className = 'stable'
+      this.status = 'Stable'
+    }
+    return className
+  }
+
+  name = () => {
+    let className = ''
+    if (this.count > 10) {
+      className = 'highlight'
+    }
+    else if (this.count < 10) {
+      className = 'fade-color'
+    }
+    return className
   }
 }
